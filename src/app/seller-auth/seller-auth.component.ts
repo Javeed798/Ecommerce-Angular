a88640dc957075429a2b6db6438c5be421269800
Javeed798 <karishmavahida@gmail.com>
2023-02-21 22:14:35 +0530

    Integrated login functionality and also integrated the api for login

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -13,11 +13,30 @@ export class SellerAuthComponent implements OnInit{
   constructor(private seller:SellerService, private router:Router) {
   }
 
+  authError:string = ''
+
+  showLogin = false;
+
   // after setting the data in localstorage when we refresh it must not go
   ngOnInit(): void {
     this.seller.reloadSeller()
   }
   signup(data:signup):void{
     this.seller.userSignUp(data)
+  }
+  openLogin(){
+    this.showLogin = true
+  }
+  openSignup(){
+    this.showLogin = false
+  }
+  login(data:signup):void{
+    this.authError=""
+    this.seller.userLogin(data)
+    this.seller.isLoginError.subscribe((error) => {
+      if (error){
+        this.authError = "Email or password is Invalid"
+      }
+    })
   }
 }
--- src/app/services/seller.service.ts
+++ src/app/services/seller.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
+import {EventEmitter, Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import {signup} from "../data-type";
+import {login, signup} from "../data-type";
 import {BehaviorSubject} from "rxjs";
 import {Router} from "@angular/router";
 
@@ -9,7 +9,9 @@ import {Router} from "@angular/router";
 })
 export class SellerService {
   //this is using for auth guard
-  isSellerLoggedIn = new BehaviorSubject<boolean>(false)
+  isSellerLoggedIn = new BehaviorSubject<boolean>(false);
+
+  isLoginError = new EventEmitter<boolean>(false)
 
   constructor(private http:HttpClient, private router:Router) { }
 
@@ -36,4 +38,20 @@ export class SellerService {
       this.router.navigate(['seller-home'])
     }
   }
+
+  userLogin(data:login){
+    console.log("data ",data)
+    this.http.get(`http://localhost:3000/seller?email=${data.email}&password=${data.password}`,{observe:'response'})
+      .subscribe((result:any) => {
+        console.log(result)
+        if(result && result.body && result.body.length){
+          localStorage.setItem("seller",JSON.stringify(result.body))
+          this.router.navigate(['seller-home'])
+        } else {
+          this.isLoginError.emit(true)
+        }
+      })
+  }
+
+
 }
