a8b0991aab1e030108b909fb68e6eff232b32552
Javeed798 <karishmavahida@gmail.com>
2023-02-21 12:36:14 +0530

    Tilll Routing is done

---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {SellerService} from "../services/seller.service";
+import {Router} from "@angular/router";
+import {signup} from "../data-type";
+
+@Component({
+  selector: 'app-seller-auth',
+  templateUrl: './seller-auth.component.html',
+  styleUrls: ['./seller-auth.component.css']
+})
+export class SellerAuthComponent {
+
+  constructor(private seller:SellerService, private router:Router) {
+  }
+
+  signup(data:signup):void{
+    this.seller.userSignUp(data).subscribe((result) => {
+      if (result){
+        this.router.navigate(['seller-home'])
+      }
+    });
+  }
+}
