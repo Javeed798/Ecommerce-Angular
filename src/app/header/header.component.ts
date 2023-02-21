25865cc7bcfd491f379b16e51c8f2b079379d107
Javeed798 <karishmavahida@gmail.com>
2023-02-21 22:45:00 +0530

    Added the navbar according to the seller and normal user

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  constructor(private route:Router) {
+  }
+
+  menuType: String ="default"
+
+  ngOnInit(): void {
+    this.route.events.subscribe((val:any) => {
+      if (val.url){
+        // checking if the url is present and checking if the url is seller url
+        if (localStorage.getItem("seller") && val.url.includes("seller") ){
+          this.menuType="seller"
+        } else {
+          this.menuType="default"
+        }
+      }
+    })
+  }
+
+
 
 }
