965996d0274a641ba7319787e976d66071c58d6f
Javeed798 <karishmavahida@gmail.com>
2023-02-22 12:40:33 +0530

    implemented delete functionality and also add product functionality and integrated fontawesome icons

--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {ProductService} from "../services/product.service";
+import {product} from "../data-type";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-seller-add-product',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./seller-add-product.component.css']
 })
 export class SellerAddProductComponent {
+
+  sellerAddProductMessage:string|undefined
+
+  constructor(private product:ProductService,private router:Router) {
+  }
+
+  submit(data:product){
+    this.product.addProduct(data).subscribe((result) => {
+      console.log(result)
+      if (result){
+        this.sellerAddProductMessage="product is successfully added";
+        this.router.navigate(['seller-home'])
+      }
+      setTimeout(() =>
+        (this.sellerAddProductMessage = undefined)
+        ,3000)
+    });
+
+  }
 
 }
--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from "../services/product.service";
+import {product} from "../data-type";
+import { faDeleteLeft } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-seller-home',
   templateUrl: './seller-home.component.html',
   styleUrls: ['./seller-home.component.css']
 })
-export class SellerHomeComponent {
+export class SellerHomeComponent implements OnInit{
+  constructor(private product:ProductService) {
+  }
+  icon=faDeleteLeft
 
+  productList:undefined | product[]
+  deleteProductId:undefined|string=''
+  ngOnInit(): void {
+    this.list();
+  }
+
+  delete(id:number){
+    this.product.deleteProduct(id).subscribe((result) => {
+      if (result){
+        this.deleteProductId="The item deleted successfully";
+        this.list();
+      }
+      setTimeout(() => {
+        this.deleteProductId=undefined
+      },2000)
+    })
+  }
+
+  list(){
+    this.product.productList().subscribe((result) => {
+      if (result){
+        this.productList = result;
+      }
+    })
+  }
 }
